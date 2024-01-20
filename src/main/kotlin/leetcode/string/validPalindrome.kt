36a6e2db8f4ffed3afc26663aabb804007cb4adc
BlueSky <dbcjdrb1997@gmail.com>
2024-01-20 15:40:49 +0900

    add validPalindrome regular expression

--- src/main/kotlin/leetcode/string/validPalindrome.kt
+++ src/main/kotlin/leetcode/string/validPalindrome.kt
@@ -8,7 +8,7 @@ package leetcode.string
 private fun isAlphanumeric(c: Char): Boolean {
     return c.isLetterOrDigit()
 }
-private fun  validPalindrome (s:String) : Boolean{
+private fun  validPalindrome1 (s:String) : Boolean{
     // 1.Check Char is Alphabet or Number and return it
     val cleanString = s.filter { isAlphanumeric(it) }.lowercase()
     // 2.reversedString and Check between to string
@@ -16,12 +16,22 @@ private fun  validPalindrome (s:String) : Boolean{
     return cleanString == reversedString
 }
 
+private fun validPalindrome2(s: String): Boolean {
+    // Use regex to filter out non-alphanumeric characters and convert to lowercase
+    val cleanString = s.replace(Regex("[^a-zA-Z0-9]"), "").toLowerCase()
+
+    // Check if the cleaned string is a palindrome
+    return cleanString == cleanString.reversed()
+}
 fun main(){
     val s1 = "A man, a plan, a canal: Panama"
     val s2 = "race a car"
     val s3 = " "
 
-    println(validPalindrome(s1))
-    println(validPalindrome(s2))
-    println(validPalindrome(s3))
+    println(validPalindrome1(s1))
+    println(validPalindrome1(s2))
+    println(validPalindrome1(s3))
+    println(validPalindrome2(s1))
+    println(validPalindrome2(s2))
+    println(validPalindrome2(s3))
 }
