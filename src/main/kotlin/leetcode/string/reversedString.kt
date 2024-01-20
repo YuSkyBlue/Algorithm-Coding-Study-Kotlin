28736313c57b299c7d21d3a4c80ec7c41a63c3f2
BlueSky <dbcjdrb1997@gmail.com>
2024-01-20 16:16:32 +0900

    reversedString  add

---
+++ src/main/kotlin/leetcode/string/reversedString.kt
@@ -0,0 +1,28 @@
+package leetcode.string
+
+/**
+ * Reference :  https://leetcode.com/problems/reverse-string/
+ * LetCode : Valid Palindrome (344)
+ */
+private fun reverseString(s: CharArray) : Unit{
+    println(s.reversed())
+}
+/** 211 ms */
+private fun reversedChar(s: CharArray) : Unit{
+    var start = 0
+    var end = s.size -1
+    while(start < end){
+        s[start] = s[end].also { s[end] = s[start] }
+//        val temp = s[start];
+//        s[start] = s[end]
+//        s[end] = temp
+        start++
+        end--
+    }
+
+}
+fun main(){
+   val  s = charArrayOf('h','e','l','l','o')
+//    reverseString(s)
+    println(reversedChar(s))
+}
--- src/main/kotlin/leetcode/string/validPalindrome.kt
+++ src/main/kotlin/leetcode/string/validPalindrome.kt
@@ -8,7 +8,9 @@ package leetcode.string
 private fun isAlphanumeric(c: Char): Boolean {
     return c.isLetterOrDigit()
 }
-private fun  validPalindrome1 (s:String) : Boolean{
+
+/** Speed 180MS */
+private fun validPalindrome1(s: String): Boolean {
     // 1.Check Char is Alphabet or Number and return it
     val cleanString = s.filter { isAlphanumeric(it) }.lowercase()
     // 2.reversedString and Check between to string
@@ -16,6 +18,7 @@ private fun  validPalindrome1 (s:String) : Boolean{
     return cleanString == reversedString
 }
 
+/** Speed 233MS */
 private fun validPalindrome2(s: String): Boolean {
     // Use regex to filter out non-alphanumeric characters and convert to lowercase
     val cleanString = s.replace(Regex("[^a-zA-Z0-9]"), "").toLowerCase()
@@ -23,7 +26,29 @@ private fun validPalindrome2(s: String): Boolean {
     // Check if the cleaned string is a palindrome
     return cleanString == cleanString.reversed()
 }
-fun main(){
+
+/** Speed 150MS */
+private fun bestSpeed(s: String): Boolean {
+    var left = 0
+    var right = s.length - 1
+    while (left < right) {
+        while (left < right && !Character.isLetterOrDigit(s[left])) {
+            left++
+        }
+        while (left < right && !Character.isLetterOrDigit(s[right])) {
+            right--
+        }
+
+        if (Character.toLowerCase(s[left]) != Character.toLowerCase(s[right])) {
+            return false
+        }
+        left++
+        right--
+    }
+    return true
+}
+
+fun main() {
     val s1 = "A man, a plan, a canal: Panama"
     val s2 = "race a car"
     val s3 = " "
@@ -31,7 +56,12 @@ fun main(){
     println(validPalindrome1(s1))
     println(validPalindrome1(s2))
     println(validPalindrome1(s3))
+
     println(validPalindrome2(s1))
     println(validPalindrome2(s2))
     println(validPalindrome2(s3))
+
+    println(bestSpeed(s1))
+    println(bestSpeed(s2))
+    println(bestSpeed(s3))
 }
